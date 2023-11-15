31d73a41751211bf31937ba3f89498ccea76d70c
ed <vercaemereduard@gmail.com>
2023-11-15 16:23:40 -0600

    wip: reports ui

--- src/routes/reports/index.tsx
+++ src/routes/reports/index.tsx
@@ -1,8 +1,22 @@
 import { component$ } from "@builder.io/qwik";
-import { type DocumentHead } from "@builder.io/qwik-city";
+import { type DocumentHead, routeLoader$ } from "@builder.io/qwik-city";
+
+export const useReports = routeLoader$(async ({ platform }) => {
+  const list = await platform.R2.list();
+  return list.objects.map((object) => ({
+    name: object.key,
+    date: object.uploaded,
+  }));
+});
 
 export default component$(() => {
-  return <></>;
+  const reports = useReports();
+
+  return (
+    <>
+      <p>{JSON.stringify(reports)}</p>
+    </>
+  );
 });
 
 export const head: DocumentHead = {
