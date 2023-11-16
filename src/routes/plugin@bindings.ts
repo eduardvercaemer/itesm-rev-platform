5f6cd52b631d621998dbe41898d71ed9eb21ec60
ed <vercaemereduard@gmail.com>
2023-11-16 15:51:35 -0600

    update bindings

--- src/routes/plugin@bindings.ts
+++ src/routes/plugin@bindings.ts
@@ -2,9 +2,7 @@ import { type RequestHandler } from "@builder.io/qwik-city";
 
 declare global {
   interface QwikCityPlatform {
-    D1: D1Database;
     R2: R2Bucket;
-    KV: KVNamespace;
     SENSORS: AnalyticsEngineDataset | null;
   }
 }
@@ -27,8 +25,6 @@ const getAnalytics = async (platform: QwikCityPlatform, name: string) => {
 };
 
 export const onRequest: RequestHandler = async ({ platform }) => {
-  platform.D1 = await getBinding<D1Database>(platform, "DATA");
   platform.R2 = await getBinding<R2Bucket>(platform, "STORAGE");
-  platform.KV = await getBinding<KVNamespace>(platform, "KV");
   platform.SENSORS = await getAnalytics(platform, "SENSORS");
 };
